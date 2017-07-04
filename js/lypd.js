a29c9aacc0aea23eda8496b959135d360360e895
wangGFF <1615823260@qq.com>
2017-07-04 10:59:52 +0800

    .

--- js/lypd.js
+++ js/lypd.js
@@ -7,52 +7,41 @@ window.onload = function () {
   creataList(data);
 };
 function creataList(data) {
-  var box = document.getElementById("footer");
+  var box = document.getElementById("list");
   for (var i = 0; i < data.length; i++) {
-    var div = document.createElement("div");
-    div.className = "row";
-    box.appendChild(div);
-
-    var divs = document.createElement("div");
-    divs.className = "col-xs-4 col-sm-2";
-    div.appendChild(divs);
-    var divss = document.createElement("div");
-    divss.className = "col-xs-8 col-sm-10";
-    div.appendChild(divss);
+    
+    var a = document.createElement("a");
+    a.className = "item item-thumbnail-left";
+    a.href = "#";
+    box.appendChild(a);
 
     var img = document.createElement("img");
-    img.id = "img";
     img.src = "img/"+ data[i].img + ".png";
-    divs.appendChild(img);
+    a.appendChild(img);
 
-    var divt = document.createElement("div");
-    divss.appendChild(divt);
+    var h2 = document.createElement("h2");
+    h2.id = "h2";
+    h2.innerHTML = data[i].name;
+    a.appendChild(h2);
     var img1 = document.createElement("img");
     img1.id = "img1";
     img1.src = "img/"+ data[i].sex + ".png";
-    divt.appendChild(img1);
-    var h2 = document.createElement("span");
-    h2.id = "h2";
-    h2.innerHTML = data[i].name;
-    divt.appendChild(h2);
+    h2.appendChild(img1);
 
     var p = document.createElement("p");
     p.id = "p";
     p.innerHTML = data[i].detail;
-    divss.appendChild(p);
-
-    var divb = document.createElement("div");
-    divss.appendChild(divb);
+    a.appendChild(p);
+    
+    var p1 = document.createElement("p");
+    p1.id = "p1";
+    p1.innerHTML = data[i].address;
+    a.appendChild(p1);
     var img2 = document.createElement("img");
     img2.id = "img2";
     img2.src = "img/"+ data[i].address_img + ".png";
-    divb.appendChild(img2);
-    var p1 = document.createElement("span");
-    p1.id = "p1";
-    p1.innerHTML = data[i].address;
-    divb.appendChild(p1);
-
-
+    p1.appendChild(img2);
+    
   }
 console.log(box);
 }
@@ -90,5 +79,5 @@ $(function () {
     var uls = $("ul")[0];
     console.log(uls);
   }
-
+  
 });
