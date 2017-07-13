af815f335318200606079c249429c613ca9edbb5
125zhangyulin <z553795430@sina.com>
2017-07-13 11:49:07 +0800

    设置大部分
    设置大部分

--- js/mySetPetInforAdd.js
+++ js/mySetPetInforAdd.js
@@ -2,5 +2,21 @@ $(function(){
 	var scHeight=window.screen.availHeight;
 	var topHeight=$(".top_sickness").height();
 	$("#petInforAdd").css("height",scHeight-topHeight+"px");
+	var liWidth=$("#petInforAdd>form>ul li").width();
+	var sexWidth=$(".sexWord").width();
+	var petSexLeft=liWidth*0.05+sexWidth;
+	$(".petSex").css("left",petSexLeft+"px");
+	$(".circleBox").css("left",petSexLeft+"px");
+	$("#petInforDl").click(function(){
+		$(".bigDeleteBox").css("display","block");
+	})
+	$("#centerDelete").click(function(){
+		$("#petInforAdd>form>ul>li>input").val("");
+		$(".bigDeleteBox").css("display","none");
+		$(".petHead").attr("src", "");
+	});
+	$("#cancelDelete").click(function(){
+		$(".bigDeleteBox").css("display","none");
+	})
 
 })
---
+++ js/setViewBack.js
@@ -0,0 +1,6 @@
+
+$(function(){
+	var height=window.screen.availHeight;
+	var topHeight=$(".top_sickness").height();
+	$(".viewBackBox").css("height",height-topHeight+"px");
+})
